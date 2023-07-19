9f71de2627d87d54f0f22b0031247e65ebf3b488
grimbinary <p4698757@honeypot-4.us-west3-a.c.ambient-climate-393117.internal>
2023-07-19 20:11:07 +0000

    Updated script to support more distros

--- quickcrowd.sh
+++ quickcrowd.sh
@@ -36,25 +36,48 @@ else
   echo "Unsupported operating system"
 fi
 
-# Install CrowdSec agent based on operating system
+# Check for Linux distributions
 if [ "$OS" == "Linux" ]; then
-  echo -e "\e[1;33mInstalling CrowdSec agent on Linux\e[0m"
-  curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-  sudo apt-get install -y crowdsec
+    # Determine the exact distribution
+    DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
-  # Prompt user to enter their enroll token and enroll the agent on the cscli console
-  read -p "Enter your enroll token: " ENROLL_TOKEN
-  sudo cscli console enroll $ENROLL_TOKEN
- echo -e "\e[1;33mPlease go back to CrowdSec to accept your enrollment. \e[0m"
-sleep 10
+    # Install CrowdSec agent based on distribution
+    if [[ "$DISTRO" == *"Ubuntu"* ]] || [[ "$DISTRO" == *"Debian"* ]]; then
+        echo -e "\e[1;33mInstalling CrowdSec agent on Debian/Ubuntu\e[0m"
+        curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
+        sudo apt-get install -y crowdsec
+    elif [[ "$DISTRO" == *"CentOS"* ]] || [[ "$DISTRO" == *"RHEL"* ]] || [[ "$DISTRO" == *"Amazon Linux"* ]]; then
+        echo -e "\e[1;33mInstalling CrowdSec agent on RHEL/CentOS/Amazon Linux\e[0m"
+        curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.rpm.sh | sudo bash
+        sudo yum install -y crowdsec
+    else
+        echo "Unsupported Linux distribution"
+    fi
 
 elif [ "$OS" == "Darwin" ]; then
-  echo -e "\e[1;33mInstalling CrowdSec agent on macOS using Docker\e[0m"
-  alias cscli="docker run --rm --entrypoint /usr/local/bin/cscli  -v /path/to/crowdsec/config.yaml:/etc/crowdsec/config.yaml -v /path/to/local_api_credentials.yaml:/etc/crowdsec/local_api_credentials.yaml -e DISABLE_ONLINE_API=true -e DISABLE_AGENT=true crowdsecurity/crowdsec:latest"
-
+    echo -e "\e[1;33mInstalling CrowdSec agent on macOS using Docker\e[0m"
+    docker run -d -v acquis.yaml:/etc/crowdsec/acquis.yaml -e COLLECTIONS="crowdsecurity/sshd" -v /var/log/auth.log:/var/log/auth.log -v /path/mycustom.log:/var/log/mycustom.log --name crowdsec crowdsecurity/crowdsec
+elif [ "$OS" == "FreeBSD" ]; then
+    echo -e "\e[1;33mInstalling CrowdSec agent on FreeBSD\e[0m"
+    pkg install crowdsec
 else
-  echo "Unsupported operating system"
+    echo "Unsupported operating system"
 fi
+
+# Prompt user to enter their enroll token and enroll the agent on the cscli console
+read -p "Enter your enroll token: " ENROLL_TOKEN
+sudo cscli console enroll $ENROLL_TOKEN
+echo -e "\e[1;33mPlease go back to CrowdSec to accept your enrollment. \e[0m"
+
+# Prompt user to confirm if they have added their enroll token to the CrowdSec site
+CONFIRM='n'
+while [[ "$CONFIRM" != "y" ]]; do
+  read -p "Have you added your enroll token to the CrowdSec site (y/n)? " CONFIRM
+  if [ "$CONFIRM" == "n" ]; then
+    echo -e "\e[1;33mWaiting for 90 seconds before asking again... \e[0m"
+    sleep 90
+  fi
+done
 
 # Completion message
 echo -e "\e[1;33mScript completed. Executing 'cscli' command to verify successful installation... \e[0m"
